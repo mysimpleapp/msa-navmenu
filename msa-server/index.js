57680cdd0f7fb18f54c46fe031b5e7e768508b8d
Nicoco220983 <nicolas.carrez@gmail.com>
2017-04-02 12:11:02 +0200

    Rename App into Msa + sheet redesign + msa-config deco

--- msa-server/index.js
+++ msa-server/index.js
@@ -1,4 +1,5 @@
-var navmenuApp = module.exports = App.subApp()
+var msaNavmenu = module.exports = {}
+var subApp = msaNavmenu.subApp = Msa.subApp("/navmenu")
 
 // require userApp, as default header use user-login box
 require("../../msa-user/msa-server")
@@ -6,37 +7,59 @@ require("../../msa-user/msa-server")
 var sheetApp = require("../../msa-sheet/msa-server")
 
 // register navmenu sheets
-sheetApp.registerSheet("navmenu", {
-	creatableBy: {group: "admin"},
-	boxType: "boxes",
+sheetApp.registerType("navmenu")
+
+// create default navmenu
+sheetApp.createSheet("navmenu", "header", {
 	content: {
-		owner: {group: "admin"},
-		box: {
-			style: { "margin":"0", "height":"100px", "border-bottom": "4px solid #4C9CF1", "box-shadow": "0 0 3px 2px #aaa" },
-			boxes: {
-				"1": {
-					type: "text",
-					style: { "position":"absolute", "top":"10px", "left":"30px" },
-					html: "<a href='/' style='color:#4C9CF1; font-size: 36px; font-weight:bold'>MySimpleApp</a>"
+		tag: "msa-sheet-boxes",
+		style: {
+			margin:0,
+			height:'100px',
+			'border-bottom':'4px solid #4C9CF1',
+			'box-shadow':'0 0 3px 2px #aaa'
+		},
+		content: [
+			{
+				tag: "msa-sheet-text",
+				style: {
+					position:'absolute',
+					top:'10px',
+					left:'30px'
 				},
-				"2": {
-					type: "msa-user-login",
-					style: { "position":"absolute", "bottom":"10px", "right":"10px", "min-height":null }
+				content: {
+					tag:'a',
+					attrs: {
+						href:'/',
+						style:{
+							color:'#4C9CF1',
+							'font-size': '36px',
+							'font-weight':'bold'
+						}
+					},
+					content:'MySimpleApp'
+				}
+			}, {
+				tag: "msa-user-login-box",
+				style: {
+					position:'absolute',
+					bottom:'10px',
+					right:'10px'
 				}
 			}
-		}
+		]
 	}
-})
+}, null)
 
 // read //////////////////////////////////////////////////////////////
 
 var getNavmenuAsPartial = function(req, res, next) {
-	sheetApp.getSheet("navmenu", "header", {user:req.user, ifNotExist:"create", insertInDb:true, forceInsertInDb:true}, function(err, sheet){
+	sheetApp.getSheet("navmenu", "header", {user:req.user}, function(err, sheet){
 		if(err) return next(err)
 		res.partial = sheetApp.renderSheetAsHtml(sheet)
 		next()
 	})
 }
 
-navmenuApp.get("/", getNavmenuAsPartial)
-navmenuApp.getPartial = getNavmenuAsPartial
+subApp.get("/", getNavmenuAsPartial)
+msaNavmenu.getPartial = getNavmenuAsPartial
